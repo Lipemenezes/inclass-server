254664216bd4906bb539e24e3de4da7125be4afc
Felipe Menezes <lipe.menezes@live.com>
2018-05-13 22:50:29 -0300

    feat: allow create user in institutional API

--- api_inst/serializers.py
+++ api_inst/serializers.py
@@ -1,8 +1,20 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import User
 from rest_framework import serializers
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups')
+        fields = ('url', 'username', 'email', 'groups', 'password')
+        write_only_fields = ('password', )
+        read_only_fields = ('id', )
--- api_inst/views.py
+++ api_inst/views.py
@@ -4,7 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 from api_inst.serializers import UserSerializer
@@ -14,3 +15,4 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated, IsAdminUser, )
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+
