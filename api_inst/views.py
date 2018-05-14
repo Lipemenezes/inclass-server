933e16bd40df408877f81187d01e78f1c441e6b3
Felipe Menezes <lipe.menezes@live.com>
2018-05-13 22:23:14 -0300

    feat: on inst api - users set permissions

--- api_inst/views.py
+++ api_inst/views.py
@@ -5,12 +5,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
 from api_inst.serializers import UserSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
+    permission_classes = (IsAuthenticated, IsAdminUser, )
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
