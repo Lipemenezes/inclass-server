0c2144fdd20710257caf1829cc0c81c2ae2915c0
Felipe Menezes <lipe.menezes@live.com>
2018-05-15 23:57:15 -0300

    feat: implement django filter to inst api

--- api_inst/serializers.py
+++ api_inst/serializers.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from inclass_server.models import Institution, Dispute, Absence, Lecture, Group, Person, Subject, Course, Address
+
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-
     def create(self, validated_data):
         user = User.objects.create(
             username=validated_data['username']
@@ -15,6 +16,74 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = User
-        fields = ('url', 'username', 'email', 'groups', 'password')
-        write_only_fields = ('password', )
+        fields = '__all__'
         read_only_fields = ('id', )
+        extra_kwargs = {
+            'password': {
+                'write_only': True
+            }
+        }
+
+
+class InstitutionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Institution
+        fields = '__all__'
+        read_only_fields = ('id', )
+
+
+class AddressSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Address
+        fields = '__all__'
+        read_only_fields = ('id', )
+
+
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+        read_only_fields = ('id', )
+
+
+class SubjectSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Subject
+        fields = '__all__'
+        read_only_fields = ('id', )
+
+
+class PersonSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Person
+        fields = '__all__'
+        read_only_fields = ('id', )
+
+
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
+        read_only_fields = ('id', )
+
+
+class LectureSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Lecture
+        fields = '__all__'
+        read_only_fields = ('id', )
+
+
+class AbsenceSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Absence
+        fields = '__all__'
+        read_only_fields = ('id', )
+
+
+class DisputeSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Dispute
+        fields = '__all__'
+        read_only_fields = ('id', )
+
--- api_inst/views.py
+++ api_inst/views.py
@@ -1,18 +1,91 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets, status
-from rest_framework.decorators import api_view
+from django.contrib.auth.models import User
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
-from api_inst.serializers import UserSerializer
+from api_inst.serializers import UserSerializer, InstitutionSerializer, AddressSerializer, CourseSerializer, \
+    SubjectSerializer, PersonSerializer, GroupSerializer, LectureSerializer, AbsenceSerializer, DisputeSerializer
+from inclass_server.models import Address, Institution, Course, Subject, Person, Group, Lecture, Absence, Dispute
 
 
 class UserViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated, IsAdminUser, )
-    queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+    queryset = User.objects.all()
+    filter_backends = (DjangoFilterBackend, )
+    filter_fields = '__all__'
 
+
+class InstitutionViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, IsAdminUser, )
+    serializer_class = InstitutionSerializer
+    queryset = Institution.objects.all()
+    filter_backends = (DjangoFilterBackend, )
+    filter_fields = '__all__'
+
+
+class AddressViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, IsAdminUser, )
+    serializer_class = AddressSerializer
+    queryset = Address.objects.all()
+    filter_backends = (DjangoFilterBackend, )
+    filter_fields = '__all__'
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, IsAdminUser, )
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+    filter_backends = (DjangoFilterBackend, )
+    filter_fields = '__all__'
+
+
+class SubjectViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, IsAdminUser, )
+    serializer_class = SubjectSerializer
+    queryset = Subject.objects.all()
+    filter_backends = (DjangoFilterBackend, )
+    filter_fields = '__all__'
+
+
+class PersonViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, IsAdminUser, )
+    serializer_class = PersonSerializer
+    queryset = Person.objects.all()
+    filter_backends = (DjangoFilterBackend, )
+    filter_fields = '__all__'
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, IsAdminUser, )
+    serializer_class = GroupSerializer
+    queryset = Group.objects.all()
+    filter_backends = (DjangoFilterBackend, )
+    filter_fields = '__all__'
+
+
+class LectureViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, IsAdminUser, )
+    serializer_class = LectureSerializer
+    queryset = Lecture.objects.all()
+    filter_backends = (DjangoFilterBackend, )
+    filter_fields = '__all__'
+
+
+class AbsenceViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, IsAdminUser, )
+    serializer_class = AbsenceSerializer
+    queryset = Absence.objects.all()
+    filter_backends = (DjangoFilterBackend, )
+    filter_fields = '__all__'
+
+
+class DisputeViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, IsAdminUser, )
+    serializer_class = DisputeSerializer
+    queryset = Dispute.objects.all()
+    filter_backends = (DjangoFilterBackend, )
+    filter_fields = '__all__'
